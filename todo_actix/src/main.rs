94b8bdbca0eb156782112a22cfc447036f20f76c
Raghav Dutta <raghav.l.ldutt@gmail.com>
2024-04-21 11:56:58 +0000

    sending json web actix server

--- todo_actix/src/main.rs
+++ todo_actix/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+use actix_web::{HttpServer, App, web, Responder, HttpResponse};
+use crate::models::Status;
+use std::io;
+
+async fn status() -> impl Responder {
+    HttpResponse::Ok()
+    .json(Status {status: "OK".to_string()})
+}
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    println!("hello this is starting port 8080");
+    HttpServer::new(|| {
+        App::new()
+           .route("/", web::get().to(status))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
