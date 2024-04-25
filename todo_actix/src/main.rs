1cfa2843534f589a75326a8b2a9efef9b2eb7877
Raghav Dutta <raghav.l.ldutt@gmail.com>
2024-04-25 13:13:01 +0000

    dotenv and configuration is up

--- todo_actix/src/main.rs
+++ todo_actix/src/main.rs
@@ -1,7 +1,9 @@
 mod models;
+mod config;
 use actix_web::{HttpServer, App, web, Responder, HttpResponse};
 use crate::models::Status;
 use std::io;
+use dotenv::dotenv;
 
 async fn status() -> impl Responder {
     HttpResponse::Ok()
@@ -9,12 +11,20 @@ async fn status() -> impl Responder {
 }
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-    println!("hello this is starting port 8080");
+
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().unwrap();
+
+    let addr = format!("http://{}:{}/", config.server.host, config.server.port);
+
+    println!("hello this is starting at {}", addr);
+    
     HttpServer::new(|| {
         App::new()
            .route("/", web::get().to(status))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
     .await
 }
