ecfd24d98191b48094a6f055d08a4c4bc654a914
Raghav Dutta <raghav.l.ldutt@gmail.com>
2024-04-26 13:29:51 +0000

    deadpool configured for database

---
+++ todo_actix/src/handler.rs
@@ -0,0 +1,18 @@
+use actix_web::{web, Responder, HttpResponse};
+use crate::models::Status;
+use deadpool_postgres::{Pool, Client};
+use crate::db;
+
+pub async fn status() -> impl Responder {
+    HttpResponse::Ok()
+    .json(Status {status: "OK".to_string()})
+}
+pub async fn get_todos(db_pool: web::Data<Pool>) -> impl Responder {
+    let client = db_pool.get().await.expect("Error connecting to database");
+    let todos = db::get_todos(&client).await;
+    
+    match result {
+        Ok(todos) => HttpResponse::Ok().json(todos),
+        Err(e) => HttpResponse::InternalServerError().into
+    }
+}
--- todo_actix/src/main.rs
+++ todo_actix/src/main.rs
@@ -1,14 +1,15 @@
 mod models;
 mod config;
+mod handler;
+mod db;
+
 use actix_web::{HttpServer, App, web, Responder, HttpResponse};
 use crate::models::Status;
 use std::io;
 use dotenv::dotenv;
+use tokio_postgres::NOTls;
+use crate::handler::*;
 
-async fn status() -> impl Responder {
-    HttpResponse::Ok()
-    .json(Status {status: "OK".to_string()})
-}
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
 
@@ -16,13 +17,17 @@ async fn main() -> io::Result<()> {
 
     let config = crate::config::Config::from_env().unwrap();
 
+    let pool = config.pg.create_pool(NOTls).unwrap();
+
     let addr = format!("http://{}:{}/", config.server.host, config.server.port);
 
     println!("hello this is starting at {}", addr);
     
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+           .data(pool.clone())
            .route("/", web::get().to(status))
+           .route("/todos{_:/?}", web::get().to(get_todos))
     })
     .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
